b3cb2b29e70f7617f6c24350ebf9c84cc5a8f864
Hanbyul Jo <hanbyul@nyu.edu>
2023-09-13 15:55:06 -0400

    Fetch collection id

--- app/scripts/control.tsx
+++ app/scripts/control.tsx
@@ -1,19 +1,46 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import {Leva, LevaPanel, useControls, useCreateStore } from 'leva';
+import axios from 'axios';
 
 
+const STAC_ENDPOINT = 'https://staging-stac.delta-backend.com';
 const Panel = styled.div`
   padding: 10px;
 `;
 
 export default function Control() {
 
-  const { searchID, tileMatrixSetId, format } = useControls(
-    { searchID: 'search-id', 
-    tileMatrixSetId: {value: 'WebMercatorQuad', editable: false}, 
-    format: {options: ['png', 'npy', 'tif', 'jpeg', 'jpg', 'jp2', 'webp', 'pngraw']}}
+  const [collectionIdData, setCollectionIdData] = useState([]);
+  const [itemId, setItemId] = useState([]); // do not make item id editable
+
+
+  const { collectionId, searchID, tileMatrixSetId, format } = useControls(
+    { collectionId: {options: collectionIdData},
+      searchID: 'search-id', 
+      tileMatrixSetId: {value: 'WebMercatorQuad'}, 
+      format: {options: ['png', 'npy', 'tif', 'jpeg', 'jpg', 'jp2', 'webp', 'pngraw']}},
+      [collectionIdData]
   );
+  useEffect(() => {
+    async function fetchData() {
+      const {data:res}  = await axios.get(`${STAC_ENDPOINT}/collections`);
+      setCollectionIdData(res.collections.map(c => c.id));
+    }
+    fetchData();
+  },[]);
+
+
+
+  useEffect(() => {
+    async function fetchData() {
+      const {data:res}  = await axios.get(`${STAC_ENDPOINT}/collections/${collectionId}/items?limit=3`);
+      const firstItem = res.features[0];
+      setItemId(firstItem?.id);
+    }
+    fetchData();
+  },[collectionId]);
+
 
   
   const theme = {
@@ -29,17 +56,13 @@ export default function Control() {
       highlight3: '#FEFEFE',
       vivid1: '#ffcc00'
     }
-
   };
 
   return (
     <Panel>
       <h1> Titiler previewer</h1>
       <Leva fill flat titleBar={false} theme={theme} />
-      <p>
-        {`/mosaic/tiles/${searchID}/${tileMatrixSetId}/{z}/{x}/{y}@{scale}x.${format}`}
 
-      </p>
     </Panel>);
 
 }
